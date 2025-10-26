d1ae417beb8e86cdaf3f799bb46eaf090eae8b9a
John <welcomtotheblackroom@gmail.com>
2025-10-26 04:27:21 -0400

    Fixed some errors in Validations.py

--- SRC/Infrastructure/Validations.py
+++ SRC/Infrastructure/Validations.py
@@ -13,7 +13,7 @@ def check_numerical_value(value:int|float, min_value:int|float, max_value:int|fl
 def check_numerical_inputvalue(message: str, min_value: int|float, max_value: int|float) -> int|float:
     while True:
         value:int  = int(input(message))
-        return check_int_value(value, min_value, max_value)
+        return check_numerical_value(value, min_value, max_value)
 
 def check_str_value(value: str, list_valid_value: list[str]) -> str|None:
     try:
@@ -33,5 +33,5 @@ def check_str_inputvalue(message:str, list_valid_value: list[str]):
 
 def check_str_lenvalue(value: str, min_len:int, max_len: int):
     len_value = len(value)
-    if check_int_value(len_value, min_len, max_len) != None:
+    if check_numerical_value(len_value, min_len, max_len) != None:
         return value
